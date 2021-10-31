83be10063b88d5b61fce58c8cb64257178f886d0
greasygamerdotcom <83616777+greasygamerdotcom@users.noreply.github.com>
2021-10-31 13:15:10 -0500

    added names to login buttons

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { AlertController, LoadingController } from '@ionic/angular';
+import { AlertController, LoadingController, ToastController } from '@ionic/angular';
 import { emailValidator } from 'src/app/validators/email.validators';
 import { passwordValidator } from 'src/app/validators/password.validator';
 import { AuthenticationService } from 'src/app/services/authentication.service';
+import { Toast } from '@capacitor/core';
+import { async } from 'rxjs';
+import { AccessProviders } from '../../providers/access-providers';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -18,11 +22,19 @@ export class LoginPage implements OnInit {
     backgroundImage:
       'url(https://images.unsplash.com/photo-1511988617509-a57c8a288659?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1051&q=80)',
   };
+  alert: any;
+  toastctrl: any;
+  email: any;
+  password: any;
+  accessProviders: any;
   
 
   constructor(
     private formBuilder: FormBuilder,
     private router: Router,
+    private alertCtrl: AlertController,
+    private toastCtrl: ToastController,
+    private accsPrvds: AccessProviders,
     private alertController: AlertController,
     public loadingController: LoadingController,
     private authService: AuthenticationService
@@ -38,33 +50,44 @@ export class LoginPage implements OnInit {
   async signIn() {
     const loading = await this.loadingController.create();
     await loading.present();
+    
+    return new Promise(resolve=>{
 
-    this.authService.login(this.loginForm.value).subscribe(
+      let body = {
+            user_email: this.email,
+            user_password: this.password
 
-      async (res) => {
-
-          await loading.dismiss();
-          this.router.navigate(['tabs/explore']);
-
-      },
-      async (res) => {
-          await loading.dismiss();
-          const alert = await this.alertController.create({
-
-            header: 'Login Failed',
-              message: res.error.error,
-              buttons: ['OK'],
-                  });
-            await alert.present();
       }
-    );
-  
+      console.log(body);
+    this.accsPrvds.postData(body, 'applogin.php').subscribe((res:any)=>{
+      if(res['success'] === true) {
+        loading.dismiss();  
+        console.log(res);      
+        this.router.navigateByUrl('/tabs/explore', { replaceUrl: true });
+      }
+      else{
+        loading.dismiss();
+        this.presentToast(res.message);
+
+      
+      }
+    });
+
+  })
+}
+async presentToast(a){
+  const toast = await this.toastCtrl.create({
+    message: a,
+    duration: 1500,
+    position: 'top'
+  });
+  toast.present();
   }
 
   goBack() {
     this.router.navigate(['/welcome']);
   }
-
+ 
   async presentLoading() {
     this.loading = await this.loadingController.create({
       cssClass: 'loader-container', 
@@ -76,3 +99,5 @@ export class LoginPage implements OnInit {
     await this.loading.present();
   }
 }
+
+
