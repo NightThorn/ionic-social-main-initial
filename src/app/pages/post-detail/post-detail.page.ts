0b7668c3a56d2d955b2b063adef4a93bfb8f930c
greasygamerdotcom <83616777+greasygamerdotcom@users.noreply.github.com>
2021-12-12 17:41:11 -0600

    post details stuff

--- src/app/pages/post-detail/post-detail.page.ts
+++ src/app/pages/post-detail/post-detail.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
+import moment from 'moment';
 import { DataService } from 'src/app/services/data.service';
 
 @Component({
@@ -16,6 +17,7 @@ export class PostDetailPage implements OnInit {
   data: any;
   post: any;
   comments: any;
+  offset: number;
 
   constructor(private fb: FormBuilder, private route: ActivatedRoute, private dataService: DataService, private router: Router) {
     this.route.queryParams.subscribe(params => {
@@ -28,9 +30,19 @@ export class PostDetailPage implements OnInit {
   ngOnInit() {
     this.dataService.getPostDetails(this.data).subscribe(res => {
       this.post = res.message;
+      for (let i = 0; i < this.post.length; i++) {
+        this.offset = moment().utcOffset();
+
+        this.post[i]['time'] = moment.utc(this.post[i]['time']).fromNow();
+      }
     })
     this.dataService.getPostComments(this.data).subscribe(res => {
       this.comments = res.message;
+      for (let i = 0; i < this.comments.length; i++) {
+        this.offset = moment().utcOffset();
+
+        this.comments[i]['time'] = moment.utc(this.comments[i]['time']).fromNow();
+      }
     })
     this.commentForm = this.fb.group({
       comment: [null]
@@ -41,5 +53,13 @@ export class PostDetailPage implements OnInit {
 
   }
 
-  
+  user(id) {
+    let navigationExtras: NavigationExtras = {
+      queryParams: {
+        special: JSON.stringify(id)
+      }
+    };
+    this.router.navigate(['/user'], navigationExtras);
+
+  }
 }
