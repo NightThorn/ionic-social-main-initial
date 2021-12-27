49177f829771365fa10087a8bdea63badc89bcdb
greasygamerdotcom <greasygamer@greasygamer.com>
2021-12-27 16:41:58 -0600

    zxv

--- src/app/pages/shop/badges/badges.page.ts
+++ src/app/pages/shop/badges/badges.page.ts
@@ -8,12 +8,21 @@ import { DataService } from 'src/app/services/data.service';
 })
 export class BadgesPage implements OnInit {
   badges: any;
+  pro: any;
 
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
     this.dataService.getBadgeShop().subscribe(res => {
       this.badges = res.message;
+      for (let i = 0; i < this.badges.length; i++) {
+        if (this.badges[i]['price'] == '0.00' && this.badges[i]['pro_only'] == '0') {
+          this.badges[i]['price'] = "Free";
+
+        } else if (this.badges[i]['price'] == '0.00' && this.badges[i]['pro_only'] == '1') {
+          this.badges[i]['price'] = "Pro Exclusive";
+        }
+      }
     });
   }
 }
