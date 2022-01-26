39882347144be423038ba87db901b93319e1c219
greasygamerdotcom <83616777+greasygamerdotcom@users.noreply.github.com>
2022-01-26 13:07:40 -0600

    started on contacts page

--- src/app/pages/contacts/contacts.page.ts
+++ src/app/pages/contacts/contacts.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { NavigationExtras, Router } from '@angular/router';
+import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
+import { StoredUser } from 'src/app/models/stored-user';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 import { DataService } from 'src/app/services/data.service';
+import { ProfileService } from 'src/app/services/profile.service';
 
 @Component({
   selector: 'app-contacts',
@@ -9,13 +12,66 @@ import { DataService } from 'src/app/services/data.service';
 })
 export class ContactPage implements OnInit {
   users: any;
+  res: any = [];
+  public dataL: Array<object> = [];
+  public friends: any = [];
+  activatedroute: any;
+  data: any;
+  public searchTerm: string = "";
+  public items: any;
+  private topLimit: number = 15;
+  public dataList: any = [];
+  navCtrl: any;
+  id;
+  me: number;
+  activeStoredUserSubscription$: any;
+  constructor(private route: ActivatedRoute, private authService: AuthenticationService, private profileService: ProfileService, private router: Router) {
 
-  constructor(private router: Router, private dataService: DataService) {}
+    this.route.queryParams.subscribe(params => {
+      if (params && params.special) {
+        this.data = JSON.parse(params.special);
+      }
+    });
 
+  }
   ngOnInit() {
+    this.activeStoredUserSubscription$ = this.authService.activeStoredUser.subscribe((storedUser: StoredUser) => {
+      this.me = storedUser.UserID;
+
+      this.profileService.fetchFriends(this.me).subscribe(res => {
+        this.friends = res.message;
+        this.dataList = this.friends.slice(0, this.topLimit);
+
+      });
+    });
+    this.setFilteredItems();
+
+  }
+  loadData(event) {
+    setTimeout(() => {
+      this.topLimit += 10;
+      this.dataList = this.friends.slice(0, this.topLimit);
+      event.target.complete();
+      if (this.dataList.length == this.dataL.length)
+        event.target.disabled = true;
+
+    }, 500);
+
   }
 
-  navigateToChat(item) {
+  setFilteredItems() {
+    this.dataList = this.filterItems(this.searchTerm);
+  }
+  filterItems(searchTerm) {
+    return this.friends.filter(item => {
+      return item.user_name.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1;
+    });
+  }
+
+
+
+
+  chat(item) {
     let navigationExtras: NavigationExtras = {
       state: {
         user: item,
