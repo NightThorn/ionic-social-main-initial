a42b9b3029c947c9b3159d460594f30cf3a75cae
greasygamerdotcom <greasygamer@greasygamer.com>
2022-02-09 17:19:36 -0600

    fixed notifications menu

--- src/app/pages/notification/notification-routing.module.ts
+++ src/app/pages/notification/notification-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
     children: [
       {
         path: '',
-        redirectTo: 'notification/all',
+        redirectTo: 'all',
         pathMatch: 'full'
       },
       {
